ad529d7ed5d625f3c5436182538b65cd450c9ec8
dev03lkomavyax <149859737+dev03lkomavyax@users.noreply.github.com>
2023-11-14 14:03:09 +0530

    added privacy&policy and termsandConditions pages

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Footer from "./components/Footer/Footer";
 import HomePage from './pages/Home';
 import AboutUs from './pages/aboutUs/AboutUs';
 import ContactUs from './pages/contactUs/ContactUs';
+import PrivacyPolicy from './pages/privacy&policy/Privacy&Policy';
 import ProductPage from './pages/productPage/ProductPage';
 import CheckoutPage from './pages/checkoutPage/CheckoutPage';
 import RegisterAsAProfessional from './pages/registerAsAProfessional/RegisterAsAProfessional';
+import TermsAndConditions from "./pages/termsAndConditions/TermsAndConditions";
 
 function App() {
 
@@ -19,6 +21,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/aboutUs" element={<AboutUs />} />
         <Route path="/contactUs" element={<ContactUs />} />
+        <Route path="/termsAndConditions" element={<TermsAndConditions />} />
+        <Route path="/privacy&policy" element={<PrivacyPolicy />} />
         <Route path="/product" element={<ProductPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/registerAsProfessionals" element={<RegisterAsAProfessional />} />
