6b0720a50368ba35c198077c46ff234054b70046
easesmith <tech@mavyax.com>
2024-01-23 10:05:03 +0530

    updated code

--- src/pages/AdminPanel/orderDetails/OrderDetails.js
+++ src/pages/AdminPanel/orderDetails/OrderDetails.js
@@ -63,8 +63,9 @@ const OrderDetails = () => {
                 <div className={classes.left_div}>
                     <div className={classes.info}>
                         <div>
-                            <h4>seller name</h4>
-                            <p>seller phone: 1234567890</p>
+                            {/* <h4>seller name</h4>
+                            <p>seller phone: 1234567890</p> */}
+                            <h5>Update Status</h5>
                             <select onChange={handleChange} value={status} className={classes.select} name="status" id="status">
                                 <option value="Pending">Pending</option>
                                 <option value="OutOfDelivery">OutOfDelivery</option>
