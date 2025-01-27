97a6ccaa7eb039401c487e102843c6e38c12ba33
dev03lkomavyax <dev03lko@mavyax.com>
2025-01-27 11:20:03 +0530

    code updated

--- src/components/dateTimeModal/DateTimeModal.jsx
+++ src/components/dateTimeModal/DateTimeModal.jsx
@@ -7,13 +7,20 @@ const DateTimeModal = ({ setIsModalOpen, isModalOpen, setInfo, info, handleOnSub
         const { name, value } = e.target;
         setInfo({ ...info, [name]: value });
     }
-    
+
 
     const getMaxDate = () => {
         const today = new Date();
         today.setDate(today.getMonth() + 30);
         return format(today, "yyyy-MM-dd");
     }
+
+    const getTodaysDate = () => format(new Date(), "yyyy-MM-dd");
+
+    const getCurrentTime = () => {
+        const now = new Date();
+        return now.toTimeString().slice(0, 5); // Format as HH:mm
+    };
 
 
     return (
@@ -33,24 +40,17 @@ const DateTimeModal = ({ setIsModalOpen, isModalOpen, setInfo, info, handleOnSub
                         </div>
                         <div className={classes.mt}>
                             <label htmlFor="bookingTime">Booking Time</label>
-                            {/* <select onChange={handleOnChange} value={info.bookingTime} className={`${classes.input_box} ${classes.padding}`} name='bookingTime' id='bookingTime'>
-                                <option value="">Select time (08:00AM - 08:00PM)</option>
-                                <option value="08:00AM">08:00 AM</option>
-                                <option value="09:00AM">09:00 AM</option>
-                                <option value="10:00AM">10:00 AM</option>
-                                <option value="11:00AM">11:00 AM</option>
-                                <option value="12:00PM">12:00 PM</option>
-                                <option value="01:00PM">01:00 PM</option>
-                                <option value="02:00PM">02:00 PM</option>
-                                <option value="03:00PM">03:00 PM</option>
-                                <option value="04:00PM">04:00 PM</option>
-                                <option value="05:00PM">05:00 PM</option>
-                                <option value="06:00PM">06:00 PM</option>
-                                <option value="07:00PM">07:00 PM</option>
-                                <option value="08:00PM">08:00 PM</option>
-                            </select> */}
                             <div className={classes.input_box}>
-                                <input className={classes.input} onChange={handleOnChange} value={info.bookingTime} type="time" name="bookingTime" id="bookingTime" placeholder="Enter booking time" />
+                                <input
+                                    className={classes.input}
+                                    onChange={handleOnChange}
+                                    value={info.bookingTime}
+                                    type="time"
+                                    name="bookingTime"
+                                    id="bookingTime"
+                                    placeholder="Enter booking time"
+                                    min={info.bookingDate === getTodaysDate() ? getCurrentTime() : "00:00"}
+                                />
                             </div>
                         </div>
                         <button className={classes.button}>Proceed</button>
